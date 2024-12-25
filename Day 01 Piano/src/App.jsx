e90f076b0219a4c375814045bf91b5ecd7a24ba1
karun sharma <sharmakarun06@gmail.com>
2024-12-25 16:05:30 +0530

    Piano with Vanilla JS approach

--- Day 01 Piano/src/App.jsx
+++ Day 01 Piano/src/App.jsx
@@ -1,22 +1,67 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import logo from '../src/assets/logo image.png';
+import c6 from './assets/tones/c6.mp3';
+import d6 from './assets/tones/d6.mp3';
+import e6 from './assets/tones/e6.mp3';
+import f6 from './assets/tones/f6.mp3';
+import g6 from './assets/tones/g6.mp3';
+import a6 from './assets/tones/a6.mp3';
+import b6 from './assets/tones/b6.mp3';
+
 
 function App() {
+
+useEffect(()=>{
+  document.addEventListener("keydown",(event)=>{
+    console.log(event.key);  
+
+    switch(event.key){
+
+      case 'a': let c62 = new Audio(c6);
+                c62.play();
+                break;
+
+      case 's': let d62 = new Audio(d6);
+                d62.play();
+                break;
+
+      case 'd': let e62 = new Audio(e6);
+                e62.play();
+                break;
+
+      case 'f': let f62 = new Audio(f6);
+                f62.play();
+                break;
+
+      case 'j': let g62 = new Audio(g6);
+                g62.play();
+                break;
+
+      case 'k': let a62 = new Audio(a6);
+                a62.play();
+                break;    
+                
+      case 'l': let b62 = new Audio(b6);
+                b62.play();
+                break;
+    }                       
+  })                   
+})  
 
   return (
     <div id="keyboard" >
       <img id="logo" src={logo} alt="brand logo"/>
       <div id='keys'>
-        <div className='key'></div>
-        <div className='key black'></div>
-        <div className='key black'></div>
-        <div className='key'></div>
-        <div className='key black'></div>
-        <div className='key black'></div>
-        <div className='key black'></div>
+        <div className='key' data-key='c6' > <span>a</span></div>
+        <div className='key black' data-key='d6'><span>s</span></div>
+        <div className='key black' data-key='e6'> <span>d</span> </div>
+        <div className='key' data-key='f6'> <span>f</span> </div>
+        <div className='key black' data-key='g6'> <span>j</span> </div>
+        <div className='key black' data-key='a6'> <span>k</span> </div>
+        <div className='key black' data-key='b6'> <span>l</span> </div>
         <div className='key'></div>
         <div className='key black'></div>
         <div className='key black'></div>
